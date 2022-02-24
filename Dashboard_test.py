32b118c36f09608edea87bb826be24cabc14d9c9
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-02-24 13:50:48 -0500

    dashboard code completed but stim needs improvement

--- Dashboard_test.py
+++ Dashboard_test.py
@@ -4,115 +4,193 @@ from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+from PIL import Image
+
+from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
 
 st.set_page_config(page_title='Numisma: Diversify your crypto holdings', layout='wide')
-#st.image("./Images/Cryptos.jpeg")
+st.image("./Images/Cryptos.jpeg")
 st.title("Numisma. Crypto Index Portfolio Management")
 
-portfolios = ['Metadex Portfolio', 'Ventidex Portfolio', 'Farmdex Portfolio']
+st.markdown("""
+Numisma is a bll bla bla.....
+""")
 
-st.write("Choose from the available indexes to start you journey into minimizing yout risk")
+st.sidebar.header('Portfolio selection')
 
+portfolios_dict = {'Metadex Portfolio': {'Logo':'Images/Metadex_pie.jpg', 'Description':'The Metaverse Index is designed to capture the trend of entertainment, sports and business shifting to a virtual environment.', 'Creation':'For this Index Weight Calculation, we uses a combination of root market cap and liquidity weighting to arrive at the final index weights. We believe that liquidity is an important consideration in this space and should be considered when determining portfolio allocation.'}, 'Ventidex Portfolio':{'Logo':'Images/Ventidex_pie.jpg', 'Description':'', 'Creation':''}, 'Farmdex Portfolio':{'Logo':'Images/Farmdex_pie.jpg', 'Description':'', 'Creation':''}}
 
+sorted_portfolio = ['Metadex Portfolio', 'Ventidex Portfolio', 'Farmdex Portfolio']
+
+selected_portfolio = st.sidebar.selectbox("Available Portfolio", sorted_portfolio)
+
+st.subheader('Current Portfolio Selection: ' + selected_portfolio)
+st.image(portfolios_dict[selected_portfolio]['Logo'], width = 500)
+
+st.subheader(" ")
+st.header(f"{selected_portfolio}' Porfolio Description")
+st.write(portfolios_dict[selected_portfolio]['Description'])
+
+st.header(f"{selected_portfolio}' Creation strategy")
+st.write(portfolios_dict[selected_portfolio]['Creation'])
+
+st.markdown("---")
 
 load_dotenv("api.env")
 
 # Define and connect a new Web3 provider
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+#accounts = w3.eth.accounts
+#address = st.selectbox("Select Account", options=accounts)
 
-accounts = w3.eth.accounts
-address = st.selectbox("Select Account", options=accounts)
-st.markdown("---")
-
-################################################################################
-# Contract Helper function:
-# 1. Loads the contract once using cache
-# 2. Connects to the contract using the contract address and ABI
-################################################################################
-
-
+# The contracts have to be loaded separately for eack Token index
+# Load the contract once using cache
+# Connects to the contract using the contract address and ABI
+# loading contract fot --------- token index
 @st.cache(allow_output_mutation=True)
 def load_contract1():
 
     # Load the contract ABI
-    with open(Path('./Ventidex_abi.json')) as f:
+    with open(Path('./Ventidex2_abi.json')) as f:
         contract_abi = json.load(f)
 
     # Set the contract address (this is the address of the deployed contract)
-    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS2")
 
     # Get the contract
     contract = w3.eth.contract(
         address=contract_address,
-        abi=contract_abi
-    )
-
+        abi=contract_abi)
+    
     return contract
-
 
 # Load the contract
 contract1 = load_contract1()
 
+# The contracts have to be loaded separately for eack Token index
+# loading contract fot --------- token index
+def load_contract2():
+
+    # Load the contract ABI
+    with open(Path('./Register.json')) as f:
+        contract_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESSArt")
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=contract_abi)
+    
+    return contract
+
+# Load the contract
+contract2 = load_contract2()
+
+# The contracts have to be loaded separately for eack Token index
+# loading contract fot --------- token index
+def load_contract3():
+
+    # Load the contract ABI
+    with open(Path('./VdexAbi.json')) as f:
+        contract_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESSVdex")
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=contract_abi)
+    
+    return contract
+
+# Load the contract
+contract3 = load_contract3()
+
+
+#st.header('Ventidex: Composed of the top 10 crypto by market cap')
+#st.subheader('This particular index was calculated by our proprietary AI')
+
 
 ################################################################################
-# Register New Artwork
+# Buying the portfolio
 ################################################################################
-st.markdown("## Choose your index")
+st.title("Buy This Portfolio")
+
+accounts = w3.eth.accounts
+
+# Use a streamlit component to get the address of the artwork owner from the user
+address = st.selectbox("Select your wallet", accounts)
+amount = st.number_input("How many shares do you want to buy?", min_value=1, value=1, step=1)
+# Use a streamlit component to get the contract URI
+# contract_uri = st.text_input("The URI to the artwork")
+
+if st.button("Buy Now"):
+
+    # Use the contract to send a transaction to the safeMint function
+    tx_hash = contract3.functions.purchase().transact({
+        "from": address, "gas": 1000000})
+
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write("Congratulation on your purchase, Hereis your Blockchain receipt")
+    st.success(dict(receipt))
+st.markdown("---")
+
+################################################################################
+# Helper functions to pin files and json to Pinata
+################################################################################
+def pin_artwork(artwork_name, artwork_file):
+    # Pin the file to IPFS with Pinata
+    ipfs_file_hash = pin_file_to_ipfs(artwork_file)
+
+    # Build a token metadata file for the artwork
+    token_json = {
+        "name": artwork_name,
+        "image": ipfs_file_hash
+    }
+    json_data = convert_data_to_json(token_json)
+
+    # Pin the json to IPFS with Pinata
+    json_ipfs_hash = pin_json_to_ipfs(json_data)
+
+    return json_ipfs_hash
+
+
+def pin_appraisal_report(report_content):
+    json_report = convert_data_to_json(report_content)
+    report_ipfs_hash = pin_json_to_ipfs(json_report)
+    return report_ipfs_hash
+
+################################################################################
+# Register New Porfolio
+################################################################################
+st.markdown("## Register Your Portfolio")
 artwork_name = st.text_input("Enter the name of your portfolio")
-artist_name = st.text_input("Enter the token name")
-initial_appraisal_value = st.text_input("Enter the initial appraisal amount")
-artwork_uri = st.text_input("Enter the URI to the artwork")
+artist_name = st.text_input("Enter your full name")
+initial_appraisal_value = st.text_input("Enter the initial investment amount")
+#file = portfolios_dict[selected_portfolio]['Logo']
+#file = st.file_uploader("Upload Artwork", type=["jpg", "jpeg", "png"])
+file = st.camera_input("Picture recording")
+
 if st.button("Register Artwork"):
-    tx_hash = contract.functions.registerPortfolio(
+    # Use the `pin_artwork` helper function to pin the file to IPFS
+    artwork_ipfs_hash =  pin_artwork(artwork_name, file)
+    artwork_uri = f"ipfs://{artwork_ipfs_hash}"
+    tx_hash = contract2.functions.registerArtwork(
         address,
         artwork_name,
         artist_name,
         int(initial_appraisal_value),
-        artwork_uri
+        artwork_uri 
     ).transact({'from': address, 'gas': 1000000})
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
-    st.write(dict(receipt))
+    st.success(dict(receipt))
+    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
+    st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{artwork_ipfs_hash})")
 st.markdown("---")
 
 
-################################################################################
-# Appraise Art
-################################################################################
-st.markdown("## Appraise Artwork")
-tokens = contract.functions.tokenURI().call()
-token_id = st.selectbox("Choose an Art Token ID", list(range(tokens)))
-new_appraisal_value = st.text_input("Enter the new appraisal amount")
-report_uri = st.text_area("Enter notes about the appraisal")
-if st.button("Appraise Artwork"):
-
-    # Use the token_id and the report_uri to record the appraisal
-    tx_hash = contract.functions.newDistribution(
-        token_id,
-        int(new_appraisal_value),
-        report_uri
-    ).transact({"from": w3.eth.accounts[0]})
-    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    st.write(receipt)
-st.markdown("---")
-
-################################################################################
-# Get Appraisals
-################################################################################
-st.markdown("## Get the appraisal report history")
-art_token_id = st.number_input("Artwork ID", value=0, step=1)
-if st.button("Get Appraisal Reports"):
-    appraisal_filter = contract.events.Portfolio.createFilter(
-        fromBlock=0,
-        argument_filters={"tokenId": art_token_id}
-    )
-    appraisals = appraisal_filter.get_all_entries()
-    if appraisals:
-        for appraisal in appraisals:
-            report_dictionary = dict(appraisal)
-            st.markdown("### Appraisal Report Event Log")
-            st.write(report_dictionary)
-            st.markdown("### Appraisal Report Details")
-            st.write(report_dictionary["args"])
-    else:
-        st.write("This artwork has no new appraisals")
--- Numdash.py
+++ Numdash.py
@@ -6,14 +6,35 @@ from dotenv import load_dotenv
 import streamlit as st
 from PIL import Image
 
+from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
+
 st.set_page_config(page_title='Numisma: Diversify your crypto holdings', layout='wide')
-#image = Image.open("./Images/Cryptos.jpeg")
-#st.image(image, width=900)
+st.image("./Images/Cryptos.jpeg")
 st.title("Numisma. Crypto Index Portfolio Management")
 
-portfolios = ['Metadex Portfolio', 'Ventidex Portfolio', 'Farmdex Portfolio']
+st.markdown("""
+Numisma is a bll bla bla.....
+""")
 
-st.write("Choose from the available indexes to start you journey into minimizing yout risk")
+st.sidebar.header('Portfolio selection')
+
+portfolios_dict = {'Metadex Portfolio': {'Logo':'Images/Metadex_pie.jpg', 'Description':'The Metaverse Index is designed to capture the trend of entertainment, sports and business shifting to a virtual environment.', 'Creation':'For this Index Weight Calculation, we uses a combination of root market cap and liquidity weighting to arrive at the final index weights. We believe that liquidity is an important consideration in this space and should be considered when determining portfolio allocation.'}, 'Ventidex Portfolio':{'Logo':'Images/Ventidex_pie.jpg', 'Description':'', 'Creation':''}, 'Farmdex Portfolio':{'Logo':'Images/Farmdex_pie.jpg', 'Description':'', 'Creation':''}}
+
+sorted_portfolio = ['Metadex Portfolio', 'Ventidex Portfolio', 'Farmdex Portfolio']
+
+selected_portfolio = st.sidebar.selectbox("Available Portfolio", sorted_portfolio)
+
+st.subheader('Current Portfolio Selection: ' + selected_portfolio)
+st.image(portfolios_dict[selected_portfolio]['Logo'], width = 500)
+
+st.subheader(" ")
+st.header(f"{selected_portfolio}' Porfolio Description")
+st.write(portfolios_dict[selected_portfolio]['Description'])
+
+st.header(f"{selected_portfolio}' Creation strategy")
+st.write(portfolios_dict[selected_portfolio]['Creation'])
+
+st.markdown("---")
 
 load_dotenv("api.env")
 
@@ -21,13 +42,13 @@ load_dotenv("api.env")
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 #accounts = w3.eth.accounts
 #address = st.selectbox("Select Account", options=accounts)
-st.markdown("---")
 
+# The contracts have to be loaded separately for eack Token index
 # Load the contract once using cache
 # Connects to the contract using the contract address and ABI
-
+# loading contract fot --------- token index
 @st.cache(allow_output_mutation=True)
-def load_contract():
+def load_contract1():
 
     # Load the contract ABI
     with open(Path('./Ventidex2_abi.json')) as f:
@@ -44,13 +65,57 @@ def load_contract():
     return contract
 
 # Load the contract
-contract = load_contract()
+contract1 = load_contract1()
 
-portfolios = {'Metadex Portfolio': {'Logo':'Images/Ventidex_pie.jpg'}, 'Ventidex Portfolio':{'Logo':'Images/Ventidex_pie.jpg'}, 'Farmdex Portfolio':{'Logo':'Images/Ventidex_pie.jpg'}}
+# The contracts have to be loaded separately for eack Token index
+# loading contract fot --------- token index
+def load_contract2():
 
-st.header('Ventidex: Composed of the top 10 crypto by market cap')
-st.subheader('This particular index was calculated by our proprietary AI')
-st.image(portfolios['Ventidex Portfolio']['Logo'], width = 500)
+    # Load the contract ABI
+    with open(Path('./Ventidex2_abi.json')) as f:
+        contract_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS2")
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=contract_abi)
+    
+    return contract
+
+# Load the contract
+contract2 = load_contract2()
+
+# The contracts have to be loaded separately for eack Token index
+# loading contract fot --------- token index
+def load_contract3():
+
+    # Load the contract ABI
+    with open(Path('./Ventidex3_abi.json')) as f:
+        contract_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS3")
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=contract_abi)
+    
+    return contract
+
+# Load the contract
+contract3 = load_contract3()
+
+
+#st.header('Ventidex: Composed of the top 10 crypto by market cap')
+#st.subheader('This particular index was calculated by our proprietary AI')
+
+
+
+
 
 ################################################################################
 # Buying the portfolio
@@ -61,17 +126,70 @@ accounts = w3.eth.accounts
 
 # Use a streamlit component to get the address of the artwork owner from the user
 address = st.selectbox("Select your wallet", accounts)
-
+amount = st.number_input("How many shares do you want to buy?", min_value=1, value=1, step=1)
 # Use a streamlit component to get the contract URI
 # contract_uri = st.text_input("The URI to the artwork")
 
 if st.button("Buy Now"):
 
     # Use the contract to send a transaction to the safeMint function
-    tx_hash = contract.functions.safeMint(address).transact({
+    tx_hash = contract3.functions.safeMint(address, selected_portfolio).transact({
         "from": address, "gas": 1000000})
 
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.success(dict(receipt))
+st.markdown("---")
+
+################################################################################
+# Helper functions to pin files and json to Pinata
+################################################################################
+
+
+def pin_index(index_name, index_file):
+    # Pin the file to IPFS with Pinata
+    ipfs_file_hash = pin_file_to_ipfs(index_file.getvalue())
+
+    # Build a token metadata file for the artwork
+    token_json = {
+        "name": index_name,
+        "image": ipfs_file_hash
+    }
+    json_data = convert_data_to_json(token_json)
+
+    # Pin the json to IPFS with Pinata
+    json_ipfs_hash = pin_json_to_ipfs(json_data)
+
+    return json_ipfs_hash
+
+
+def pin_index_report(report_content):
+    json_report = convert_data_to_json(report_content)
+    report_ipfs_hash = pin_json_to_ipfs(json_report)
+    return report_ipfs_hash
+
+
+################################################################################
+# Register New Artwork
+################################################################################
+st.markdown("## Register Your Portfolio")
+index_name = st.text_input("Enter the name of the artwork")
+holder_name = st.text_input("Enter your name")
+initial_index_value = st.text_input("Enter the initial index amount")
+file = st.file_uploader("Upload Artwork", type=["jpg", "jpeg", "png"])
+if st.button("Register Portfolio"):
+    # Use the `pin_artwork` helper function to pin the file to IPFS
+    index_ipfs_hash =  pin_index(index_name, file)
+    index_uri = f"ipfs://{index_ipfs_hash}"
+    tx_hash = contract3.functions.registerPortfolio(
+        address,
+        index_name,
+        holder_name,
+        int(initial_index_value)
+    ).transact({'from': address, 'gas': 1000000})
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
+    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
+    st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{index_ipfs_hash})")
 st.markdown("---")
